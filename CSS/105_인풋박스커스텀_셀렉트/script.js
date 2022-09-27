ff6f803ffe86852cf3045729a06c8e40b13e57d0
minzyee <vvzmsqufvv@gmail.com>
2022-09-28 00:41:00 +0900

    강의 내용 파일 업로드

---
+++ CSS/105_인풋박스커스텀_셀렉트/script.js
@@ -0,0 +1,6 @@
+let btnSelectTitle = document.querySelector('.btn-select');
+let listMember = document.querySelector('.list-member');
+
+btnSelectTitle.addEventListener('click', () => {
+  btnSelectTitle.classList.toggle('on');
+});
