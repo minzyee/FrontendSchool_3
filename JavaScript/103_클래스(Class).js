3a7a2464dec0c284259df513bfd9281667153802
minzyee <vvzmsqufvv@gmail.com>
2022-11-06 14:31:32 +0900

    221106

---
+++ JavaScript/103_클래스(Class).js
@@ -0,0 +1,12 @@
+
+// function Robot(name) {
+//   this.name = name;
+// }
+
+// class robot = new Robot('개리');
+
+// robot.name;
+
+
+// 자바스크립트만의 사용자 정의 타입 생성(객체 생성) 방법을 
+// 다른 언어의 클래스 문법첯럼 바꿔준 것이 JS 클래스이다.
